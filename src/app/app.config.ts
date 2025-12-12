39d09e1ec2f4decf7080d82c03850d20ea54d115
Maged Negm <mnegm@student.tgm.ac.at>
2025-12-12 13:00:46 +0100

    Install AngularFire (and ignore .DS_Store) try again deleted line in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-
-import { environment } from '../environments/environment';
-
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage } from '@angular/fire/storage';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +17,6 @@ export const appConfig: ApplicationConfig = {
     provideFunctions(() => getFunctions()),
     provideStorage(() => getStorage()),
     provideMessaging(() => getMessaging()),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendly-chat-da466","appId":"1:519082825666:web:8353df95625eb42049285b","storageBucket":"friendly-chat-da466.firebasestorage.app","apiKey":"AIzaSyAuQuXLnH9GYZyGT3BnVhDAaEwEZhfpSOc","authDomain":"friendly-chat-da466.firebaseapp.com","messagingSenderId":"519082825666","measurementId":"G-YR81NFMY6Q","projectNumber":"519082825666","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()),
+    provideRouter(routes)
   ],
-};
+};  
